56a842daf760841fbe3ee3a697a81ad245d1d07a
Игорь Шадрин <igor@shadrins.ru>
2020-10-28 01:00:07 +0300

    Vector2, LinkType
    close #1

---
+++ ParrotProject/ParrotProject/Vector2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParrotProject
+{
+    class Vector2
+    {
+        double x, y;
+        public Vector2(double x, double y)
+        {
+            thix.x = x;
+            this.y = y;
+        }
+    }
+}
