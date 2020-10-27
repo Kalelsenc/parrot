705945eba2d0f6e9e464ff55aa2d324ffed2eab0
Арсений <nikolaychuk_a@mail.ru>
2020-10-28 02:02:10 +0300

    NeuroLay+Synaps+ANeuron+INeuron

---
+++ ParrotProject/ParrotProject/Synaps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParrotProject
+{
+    class Synaps
+    {
+        readonly int fromId;
+        readonly int toId;
+        double weight;
+
+        Synaps (int from, int to, double w)
+        {
+            fromId = from;
+            toId = to;
+            weight = w;
+        }
+
+        void setWeight(double w)
+        {
+            weight = w;
+        }
+    }
+}
