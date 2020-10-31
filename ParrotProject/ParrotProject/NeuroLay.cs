dc920b8c9bc46ed3ab62bc7f1a1465fafdffc3f1
Игорь Шадрин <igor@shadrins.ru>
2020-10-31 22:16:36 +0300

    NeuroLay interaction

--- ParrotProject/ParrotProject/NeuroLay.cs
+++ ParrotProject/ParrotProject/NeuroLay.cs
@@ -37,25 +37,15 @@ namespace ParrotProject
             synapses.Add(syn);
         }
 
-        object call(int i, List< object> value)
+        public Lookup<int, object> calculate(List<NeuroLay>.Enumerator enumerator, Lookup<int, object> groupings )
         {
-            var groupings = 
+            var neuronsOutput = from n in neurons select new { id = neurons.IndexOf(n), value = n.get(groupings[neurons.IndexOf(n)].ToList()) };
+            var returnValues = from s in synapses join c in neuronsOutput on s.fromId equals c.id select new { id = s.toId, value = s.weight != Double.NaN ? (double)c.value * s.weight : c.value };
+            var lookupList = (Lookup<int, object>)returnValues.ToLookup(p => p.id, p => p.value);
 
-            return (Lookup<int, object>)groupings.ToLookup(x=>x.id, x=>x.value);
-        }
-
-        public Lookup<int, object> descent(List<NeuroLay> list, int id)
-        {
-            if(id>0)
-            {
-                Lookup<int, object> values = list[id - 1].descent(list, id - 1);
-
-                var current =  from n in neurons select new { id = neurons.IndexOf(n), value = n.get(values[neurons.IndexOf(n)].ToList()) };
-
-                var descentReturn = from s in synapses join c in current on s.fromId equals c.id select new { id = s.toId, value = s.weight != Double.NaN ? (double)c.value * s.weight : c.value };
-                return (Lookup<int, object>)descentReturn;
-            }
-            return  (Lookup<int, object>) from n in neurons select new { id = neurons.IndexOf(n), value = n.get() };
+            if (enumerator.MoveNext())
+                return enumerator.Current.calculate(enumerator, lookupList);
+            else return lookupList;
         }
     }
 }
